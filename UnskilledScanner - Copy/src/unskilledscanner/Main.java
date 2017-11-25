4d4d6863945cc914aa92e7a9e44d5bdaeb283974
KaeLCe <lecroller@hotmail.fr>
2017-11-25 20:15:56 +0100

    Initial commit

---
+++ UnskilledScanner - Copy/src/unskilledscanner/Main.java
@@ -0,0 +1,13 @@
+package unskilledscanner;
+
+import unskilledscanner.ui.Interface;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Interface ui = new Interface();
+		ui.start();
+	}
+
+}
---
+++ UnskilledScanner/src/unskilledscanner/ui/Interface.java
@@ -0,0 +1,93 @@
+package unskilledscanner.ui;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+
+import unskilledscanner.scanner.UnskilledScanner;
+
+public class Interface {
+
+	private Scanner reader;
+	private UnskilledScanner scanner;
+	
+	public Interface(){
+		this.reader = new Scanner(System.in);
+		this.scanner = new UnskilledScanner();
+	}
+	
+   /******************
+	**     MENU     **
+	******************/
+	public void start() {
+		System.out.println("**************************");
+		System.out.println("**  Unskilled Scanner   **");
+		System.out.println("**************************");
+		System.out.println("MENU, select return liked");
+		System.out.println("--------------------------------------------------------------------------------");
+		System.out.println("  Scann Local Network  #  Open Ports List  #  Port Streaming  #  TCP Try Out");
+		System.out.println("--------------------------------------------------------------------------------");
+		System.out.println("|          1           |         2         |         3        |        4       |");
+		System.out.println();
+		System.out.print("Select you choice: ");
+		int choice = Integer.parseInt(reader.nextLine());
+		
+		if(choice == 1) {
+			scanLocalNetwork();
+		}
+		
+		if(choice == 2) {
+			openPortsList();
+		}
+		
+	}
+	
+	
+	public void scanLocalNetwork() {
+		Boolean ipFormat = true;
+		String address = "";
+		while(ipFormat) {
+			System.out.print("IP address target: ");
+			address = reader.nextLine();
+			ipFormat = checkIPFormat(address);
+		}
+		this.scanner.scanLocalNetwork(address);
+	}
+	
+   /********************************
+	**  Liste les ports ouverts   **
+	**  sur la machine ciblée     **
+	********************************/
+	public void openPortsList() {
+		Boolean ipFormat = true;
+		while(ipFormat) {
+			System.out.print("IP address target: ");
+			String address = reader.nextLine();
+			ipFormat = checkIPFormat(address);
+		}
+		String address = reader.nextLine();
+		System.out.print("Begin at port: ");
+		int portDeb = Integer.parseInt(reader.nextLine());
+		System.out.print("End at port: ");
+		int portFin = Integer.parseInt(reader.nextLine());
+		this.scanner.scanPorts(address, portDeb, portFin);
+	}
+	
+	
+   /**************************
+	**  Vérifie le format   **
+	**  de l'adresse IP     **
+	**************************/
+    public Boolean checkIPFormat(String iproot) {
+    	String nipTemp = iproot.replace('.', '-');
+        String[] nip = nipTemp.split("-");		
+        if (nip.length != 4) {
+            System.out.println("Base Ip incorrecte !!! exemple 192.168.1.1");
+            return true;
+        }else {
+        	return false;
+        }
+    }
+	
+}
